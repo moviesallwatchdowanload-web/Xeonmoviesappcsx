41f052dd3910a5e0f2ed7b9b95a61e8b5a2f6ff4
moviesallwatchdownload-web <moviesallwatchdowanload@gmail.com>
2026-09-17 17:38:39 +0530

    Fix Bollyflix resource configuration

--- Bollyflix/build.gradle.kts
+++ Bollyflix/build.gradle.kts
@@ -2,19 +2,12 @@ version = 33
 
 cloudstream {
     language = "hi"
-    // All of these properties are optional, you can safely remove them
 
     description = "Movies and Series upto 4K"
-     authors = listOf("megix")
+    authors = listOf("megix")
 
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-     * */
-    status = 1 // will be 3 if unspecified
+    status = 1
+
     tvTypes = listOf(
         "TvSeries",
         "Movie",
@@ -23,4 +16,6 @@ cloudstream {
     )
 
     iconUrl = "https://raw.githubusercontent.com/SaurabhKaperwan/CSX/refs/heads/master/Bollyflix/icon.png"
+
+    requiresResources = false
 }
